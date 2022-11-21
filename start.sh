f37d95270d0e480cb55c7017c419b8b42249d178
anuragperiwal0 <116804267+anuragperiwal0@users.noreply.github.com>
2022-11-21 16:40:30 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/RoyalKrrishna/MdiskWalaBot.git /MdiskWalaBot
+cd /MdiskWalaBot
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
