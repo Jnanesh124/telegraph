e2a4496a431de3a8e6df54c234fb7b8a717f12de
anuragperiwal0 <116804267+anuragperiwal0@users.noreply.github.com>
2022-11-21 16:54:06 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,183 @@
+from datetime import datetime
+from configs import Config
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from TeamTeleRoid.database import db
+
+
+@Client.on_message(filters.command("help") & filters.private)
+async def help_handler(_, event: Message):
+    await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([
+            [
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
+            ],
+
+             [InlineKeyboardButton("About", callback_data="About_msg"),
+             InlineKeyboardButton("Help", callback_data="Help_msg")
+             ]
+        ])
+    )
+
+@Client.on_message(filters.command('leave') & filters.private &  filters.chat(Config.BOT_OWNER))
+async def leave_a_chat(bot, message):
+    if len(message.command) == 1:
+        return await message.reply('Give me a chat id')
+    chat = message.command[1]
+    try:
+        chat = int(chat)
+    except:
+        chat = chat
+    try:
+        buttons = [[
+            InlineKeyboardButton('𝚂𝚄𝙿𝙿𝙾𝚁𝚃', url=f'https://t.me/Cyniteofficial')
+        ]]
+        reply_markup=InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=chat,
+            text='<b>Hello Friends, \nMy admin has told me to leave from group so i go! If you wanna add me again contact my support group.</b>',
+            reply_markup=reply_markup,
+        )
+
+        await bot.leave_chat(chat)
+        await message.reply(f"left the chat `{chat}`")
+    except Exception as e:
+        await message.reply(f'Error - {e}')
+
+@Client.on_message(filters.command("request") & filters.group)
+async def report_user(bot, message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        reporter = str(message.from_user.id)
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chat_id, filter="administrators")
+        success = True
+        report = f"𝖱𝖾𝗉𝗈𝗋𝗍𝖾𝗋 : {mention} ({reporter})" + "\n"
+        report += f"𝖬𝖾𝗌𝗌𝖺𝗀𝖾 : {message.reply_to_message.link}"
+        for admin in admins:
+            try:
+                reported_post = await message.reply_to_message.forward(admin.user.id)
+                await reported_post.reply_text(
+                    text=report,
+                    chat_id=admin.user.id,
+                    disable_web_page_preview=True
+                )
+                success = True
+            except:
+                pass
+        if success:
+            await message.reply_text("Your Requested Movie Name Sent To Group admins!")
+
+@Client.on_message(filters.command("users") & filters.private &  filters.chat(Config.BOT_OWNER))
+async def total_users(_, event: Message):
+    total_users = await db.total_users_count()
+    msg = f"""
+    Users: {total_users} users
+
+    """
+    await event.reply_text(msg)
+
+@Client.on_message( filters.command("start") & filters.private)
+async def start_handler(_,event: Message):
+    await event.reply_photo(
+        photo=Config.START_PHOTO,
+        caption=Config.START_MSG.format(event.from_user.mention),
+        reply_markup=InlineKeyboardMarkup([
+            [
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{Config.BOT_USERNAME}?startgroup=true')
+            ],
+
+             [InlineKeyboardButton("About", callback_data="About_msg"),
+             InlineKeyboardButton("Help", callback_data="Help_msg")
+             ]
+        ])
+    )
+
+VERIFY = {}
+@Client.on_message(filters.command("verify") & filters.group)
+async def request_handler(c,m: Message):
+    global VERIFY
+    chat_id = m.chat.id
+    user_id = m.from_user.id if m.from_user else None
+
+
+    if VERIFY.get(str(chat_id)) == None: # Make Admin's ID List
+        admin_list = []
+        async for x in c.iter_chat_members(chat_id=chat_id, filter="administrators"):
+            admin_id = x.user.id
+            admin_list.append(admin_id)
+        admin_list.append(None)
+        VERIFY[str(chat_id)] = admin_list
+
+    if not user_id in VERIFY.get(str(chat_id)): # Checks if user is admin of the chat
+        return
+
+    group_id = m.chat.id
+    group_info = await db.get_group(group_id)
+
+    if not group_info["has_access"] or not await db.is_group_verified(group_id):
+        REPLY_MARKUP = InlineKeyboardMarkup([
+            [
+                InlineKeyboardButton('Request Access', callback_data=f'request_access#{m.chat.id}#{m.from_user.id}'),
+            ],
+
+        ])
+
+        return await m.reply_text(f"Your group may not have access to add your own Database Channel or may have expired. Please Verify Again to the admin" ,reply_marku>
+
+    else:
+        return await m.reply_text("Your group already have access to /database")
+
+
+@Client.on_message(filters.command("database") & filters.group)
+async def addb_handler(c, m: Message):
+    global VERIFY
+    chat_id = m.chat.id
+    user_id = m.from_user.id if m.from_user else None
+
+
+    if VERIFY.get(str(chat_id)) == None: # Make Admin's ID List
+        admin_list = []
+        async for x in c.iter_chat_members(chat_id=chat_id, filter="administrators"):
+            admin_id = x.user.id
+            admin_list.append(admin_id)
+        admin_list.append(None)
+        VERIFY[str(chat_id)] = admin_list
+
+    if not user_id in VERIFY.get(str(chat_id)): # Checks if user is admin of the chat
+        return
+
+    group_id = m.chat.id
+    group_info = await db.get_group(str(group_id))
+
+    if group_info["has_access"] and await db.is_group_verified(group_id):
+        if len(m.command) == 2:
+            db_channel = m.command[1]
+
+
+            try:
+                invite_link =  await c.create_chat_invite_link(int(db_channel))
+            except Exception as e:
+                return await m.reply_text("Make sure you you have made the bot as admin in ur channel "+str(db_channel))
+
+
+            REPLY_MARKUP = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton('Allow DB Channel', callback_data=f'dbgive_access#{group_id}#{m.from_user.id}#{db_channel}'),
+            InlineKeyboardButton('Deny', callback_data=f'dbdeny_access#{m.from_user.id}#{db_channel}'),
+        ],
+        [
+
+            InlineKeyboardButton('Close', callback_data=f'delete'),
+        ],
+
+    ])
+
+            await c.send_message(Config.LOG_CHANNEL,  f"Join the channel and then alllow. \n\n#NewDBChannel\n\nDB Chnl Invite Link: {invite_link.invite_link}\nGroup:`{>
+            return await m.reply_text("Database Channel added successfully. Wait for the admin to approve the channel. You will be notified", )
+        else:
+            return await m.reply_text("Make the bot admin in the channel and /database -100xxx")
+    else:
+        return await m.reply_text("Your group does not have access to this command. Please /Verify access")
+
+
